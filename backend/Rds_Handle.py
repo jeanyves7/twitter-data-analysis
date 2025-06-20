e43a4cf1b84bc3a04ea25c26dff038a4b1feca55
jean yves youssef <68806505+jeanyves7@users.noreply.github.com>
2025-06-20 15:32:03 +0300

    Use parameterized queries

--- backend/Rds_Handle.py
+++ backend/Rds_Handle.py
@@ -24,8 +24,8 @@ def get_available_search_engine(conn: Connection):
             print(i)
         data = data[0]
         cur = conn.cursor()
-        query = "Update engine_search set status='true' where name='{}'".format(data)
-        cur.execute(query)
+        query = "Update engine_search set status='true' where name=%s"
+        cur.execute(query, (data,))
         conn.commit()
 
         return df['name'][0]
@@ -45,13 +45,13 @@ def ongoing_search(conn: Connection, check_email: bool, name="", email="", ):
     try:
 
         if check_email:
-            query = "Select email from ongoing_search where email='{}'".format(email)
-            df = pd.read_sql(sql=query, con=conn)
+            query = "Select email from ongoing_search where email=%s"
+            df = pd.read_sql(sql=query, con=conn, params=(email,))
             data = df['email']
 
         else:
-            query = "Select query from ongoing_search where query='{}'".format(name)
-            df = pd.read_sql(sql=query, con=conn)
+            query = "Select query from ongoing_search where query=%s"
+            df = pd.read_sql(sql=query, con=conn, params=(name,))
             data = df['query']
         if len(data) != 0:
             return True
@@ -66,8 +66,8 @@ def update_ongoing_search(name, conn: Connection, engine, email):
     cur = None
     try:
         cur = conn.cursor()
-        query = "Insert Into ongoing_search(engine,query,email) values('{0}','{1}','{2}')".format(engine, name, email)
-        cur.execute(query)
+        query = "Insert Into ongoing_search(engine,query,email) values(%s,%s,%s)"
+        cur.execute(query, (engine, name, email))
         conn.commit()
         cur.close()
         return False
@@ -85,10 +85,12 @@ def update_requested_study(study, insert: bool, conn: Connection, email=""):
         cur = conn.cursor()
         query = None
         if insert:
-            query = "INSERT INTO update_requested_study(email,study) values('{0}','{1}')".format(email, study)
+            query = "INSERT INTO update_requested_study(email,study) values(%s,%s)"
+            params = (email, study)
         else:
-            query = "DELETE FROM update_requested_study where study='{}'".format(study)
-        cur.execute(query)
+            query = "DELETE FROM update_requested_study where study=%s"
+            params = (study,)
+        cur.execute(query, params)
         conn.commit()
     except Exception as e:
         print("Error in update_requested_study : {}".format(e.__str__()))
@@ -101,9 +103,8 @@ def post_waiting_query(email, study, stop_date, conn: Connection):
     cur = None
     try:
         cur = conn.cursor()
-        query = "Insert Into waiting_query(email,query,stop_date) values('{0}','{1}','{2}')".format(email, study,
-                                                                                                    stop_date)
-        cur.execute(query)
+        query = "Insert Into waiting_query(email,query,stop_date) values(%s,%s,%s)"
+        cur.execute(query, (email, study, stop_date))
         conn.commit()
     except Exception as e:
         print("Error occurred in post_waiting_query : {}".format(e.__str__()))
@@ -119,31 +120,28 @@ def update_previous_study(study, report: bool, start: bool, conn: Connection):
         query = ""
         date = get_date()
         if report:
-            query = "UPDATE previous_studies SET report_date='{0}' where study='{1}'".format(date, study)
-            cur.execute(query)
+            query = "UPDATE previous_studies SET report_date=%s where study=%s"
+            cur.execute(query, (date, study))
             conn.commit()
 
         else:
             if start:
-                query = "INSERT INTO previous_studies(study, start_date, end_date, report_date) VALUES ('{0}','{1}',NULL,NULL)".format(
-                    study, date)
-                cur.execute(query)
+                query = "INSERT INTO previous_studies(study, start_date, end_date, report_date) VALUES (%s,%s,NULL,NULL)"
+                cur.execute(query, (study, date))
                 conn.commit()
-                query = "select id from previous_studies where study='{0}' and start_date='{1}'".format(study,
-                                                                                                        get_date())
-                df = pd.read_sql(query, conn)
+                query = "select id from previous_studies where study=%s and start_date=%s"
+                df = pd.read_sql(query, conn, params=(study, get_date()))
                 data = df.iloc[0]
                 data = data[0]
                 hashed = hashlib.sha256(int(data).to_bytes(16, 'little', signed=False)).hexdigest()
-                query = "UPDATE previous_studies set hashed='{0}' where study='{1}'".format(str(hashed), study)
-                cur.execute(query)
+                query = "UPDATE previous_studies set hashed=%s where study=%s"
+                cur.execute(query, (str(hashed), study))
                 conn.commit()
 
             else:
-                query = "UPDATE previous_studies SET end_date='{0}' where study='{1}'".format(date, study)
-                cur.execute(query)
+                query = "UPDATE previous_studies SET end_date=%s where study=%s"
+                cur.execute(query, (date, study))
                 conn.commit()
-
     except Exception as e:
         print("Error occurred in update_previous_study : {}".format(e.__str__()))
     finally:
@@ -162,8 +160,8 @@ def get_waiting_query(conn: Connection):
             study = data[1]
             stop_date = data[2]
             cur = conn.cursor()
-            query = "DELETE FROM waiting_query where email='{}'".format(email)
-            cur.execute(query)
+            query = "DELETE FROM waiting_query where email=%s"
+            cur.execute(query, (email,))
             conn.commit()
             per = get_available_search_engine(conn)
             print(per)
@@ -189,16 +187,16 @@ def close_engine_search(search_query, conn: Connection):
     cur = None
     search_query = clean([' ', '-'], search_query, '_')
     try:
-        query = " Select engine from ongoing_search where query='{}' ".format(search_query)
-        df = pd.read_sql(sql=query, con=conn)
+        query = " Select engine from ongoing_search where query=%s "
+        df = pd.read_sql(sql=query, con=conn, params=(search_query,))
         engine = df.iloc[0]
         print("closing engine:" + engine[0])
         cur = conn.cursor()
-        query = "Update engine_search set status='false' where name='{}'".format(engine[0])
-        cur.execute(query)
+        query = "Update engine_search set status='false' where name=%s"
+        cur.execute(query, (engine[0],))
         conn.commit()
-        query = "Delete from ongoing_search where query='{}'".format(search_query)
-        cur.execute(query)
+        query = "Delete from ongoing_search where query=%s"
+        cur.execute(query, (search_query,))
         conn.commit()
     except Exception as e:
         print("Error occurred in close_engine_search {}".format(e.__str__()))
@@ -260,8 +258,8 @@ def insert_analysed_data(table, number_tweets, positive, neutral, negative, comp
                                                                              str(most_retweeted[3]),
                                                                              most_retweeted[4])
 
-        query = "SELECT start_date,end_date from previous_studies where study='{}'".format(table)
-        df = pd.read_sql(query, conn)
+        query = "SELECT start_date,end_date from previous_studies where study=%s"
+        df = pd.read_sql(query, conn, params=(table,))
         data = df.iloc[0]
         start = data[0]
         end_d = data[1]
@@ -271,12 +269,13 @@ def insert_analysed_data(table, number_tweets, positive, neutral, negative, comp
         country_word = country_word[:-1]
         study_date = table + "_" + get_date().replace("-", "_")
         cur = conn.cursor()
-        query = "INSERT INTO {0}(study,start_date,end_date,number_tweets,positive,neutral,negative,compound,word_cloud,company_cloud,number_users,companies,number_influ,hashtag_cloud,countries_cloud,likes,retweets,most_liked,most_retweeted)VALUES ('{1}','{2}','{3}',{4},{5},{6},{7},'{8}','{9}','{10}',{11},{12},{13},'{14}','{15}',{16},{17},'{18}','{19}')".format(
-            study_date, table, start, end_d, number_tweets, positive, neutral, negative, compound, list_word,
-            company_word,
-            number_users,
-            companies, number_influ, hash_word, country_word, likes, retweets, most_liked_tweet, most_retweeted_tweet)
-        cur.execute(query)
+        query = f"INSERT INTO {study_date}(study,start_date,end_date,number_tweets,positive,neutral,negative,compound,word_cloud,company_cloud,number_users,companies,number_influ,hashtag_cloud,countries_cloud,likes,retweets,most_liked,most_retweeted)" \
+                " VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        params = (
+            table, start, end_d, number_tweets, positive, neutral, negative, compound, list_word, company_word,
+            number_users, companies, number_influ, hash_word, country_word, likes, retweets, most_liked_tweet,
+            most_retweeted_tweet)
+        cur.execute(query, params)
         conn.commit()
 
     except Exception as e:
@@ -290,9 +289,9 @@ def update_gender_percentage(study, male, female, conn: Connection):
     cur = None
     try:
         study_date = study + "_" + get_date().replace("-", "_")
-        query = "Update {0} set male={1}, female={2} where study='{3}'".format(study_date, male, female, study)
+        query = f"Update {study_date} set male=%s, female=%s where study=%s"
         cur = conn.cursor()
-        cur.execute(query)
+        cur.execute(query, (male, female, study))
         conn.commit()
     except Exception as e:
         print("Error occurred in update_gender_percentage {}".format(e.__str__()))
@@ -328,8 +327,8 @@ def get_previous_studies(conn: Connection):
 
 def get_hash_id(study, conn: Connection):
     try:
-        query = "SELECT hashed from previous_studies where study='{0}'".format(study)
-        df = pd.read_sql(query, conn)
+        query = "SELECT hashed from previous_studies where study=%s"
+        df = pd.read_sql(query, conn, params=(study,))
         data = df.iloc[0][0]
         return data
     except Exception as e:
@@ -356,9 +355,8 @@ def split_words(cloud, sep=" ", first="text", second="value"):
 def get_analysed_study(study_id, conn: Connection):
     conn.commit()
     try:
-        query = "select study,report_date from previous_studies where hashed='{}' and report_date is not NULL".format(
-            str(study_id))
-        df = pd.read_sql(query, conn)
+        query = "select study,report_date from previous_studies where hashed=%s and report_date is not NULL"
+        df = pd.read_sql(query, conn, params=(str(study_id),))
         report_date = str(df.iloc[0][1])
         study = str(df.iloc[0][0]) + "_" + report_date.replace("-", "_")
         query = "Select * from {}".format(study)
